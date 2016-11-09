ceb892e54e9bb4a5383be32c12f7b933e2cbdaa9
Komaron James <kjames@weblinc.com>
2016-11-09 11:06:55 -0500

    Finishing layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,6 @@
 module ApplicationHelper
   def full_title(title='')
     base_title = 'Ruby on Rails Sample App'
-
-    if (title == '')
-      base_title
-    else
-      title + ' | ' + base_title
-    end
+    title.empty? ? base_title : title + ' | ' + base_title
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,31 +7,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get root" do
-    get root_url
+    get root_path
     assert_response :success
     assert_select 'title', "#{@app_title}"
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select 'title', "#{@app_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select 'title', "Help | #{@app_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select 'title', "About | #{@app_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select 'title', "Contact | #{@app_title}"
   end
